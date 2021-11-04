a33245af0e0894156e331fc3bc170c0189e80fa5
Luiz Fernando <fernando@mixtelatacado.com.br>
2021-11-04 19:44:23 -0300

    Substituido laço while pelo for

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,9 +4,7 @@ print('*********************************')
 
 numero_secreto = 42
 total_de_tentativas = 3
-# rodada = 1
 
-# while rodada <= total_de_tentativas:
 for rodada in range(1, total_de_tentativas + 1):
     print(f'Tentativa {rodada} de {total_de_tentativas}')
     chute_str = input('Digite o seu número: ')
@@ -29,5 +27,5 @@ for rodada in range(1, total_de_tentativas + 1):
             print('Você errou! O seu chute foi maior do que o número secreto')
         elif(menor):
             print('Você errou! O seu chute foi menor do que o número secreto')
-   # rodada += 1
+
 print('Fim do jogo')
